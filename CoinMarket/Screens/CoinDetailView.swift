c1fe3dc8b27bb6178e839779b7fe345f94b79d33
Lex <lex.hrabovskyi@gmail.com>
2020-02-03 11:46:48 +0600

    added detail coin view

---
+++ CoinMarket/Screens/CoinDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  CoinDetailView.swift
+//  CoinMarket
+//
+//  Created by Александр on 03.02.2020.
+//  Copyright © 2020 Александр. All rights reserved.
+//
+
+import SwiftUI
+
+struct CoinDetailView: View {
+    
+    @EnvironmentObject private var marketService: MarketService
+    private var coin: Coin
+    
+    init(with coin: Coin) {
+        self.coin = coin
+    }
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Text(coin.symbol)
+                .bold()
+                .font(.system(size: 30))
+            
+            Text(coin.name)
+                .bold()
+                .font(.system(size: 20))
+            
+            Text("USD price is: \(coin.priceUsd)")
+            Text("BTC price is: \(coin.priceBtc)")
+            
+        }
+        .navigationBarItems(trailing: Button(action: {
+            self.marketService.updateList()
+        }) {
+            Image(systemName: "arrow.clockwise")
+        })
+        
+    }
+}
+
+struct CoinDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinDetailView(with: Coin(id: "bitcoin", name: "Bitcoin", symbol: "BTC", priceUsd: "9377.00503109", priceBtc: "1.0"))
+            .environmentObject(MarketService())
+    }
+}
